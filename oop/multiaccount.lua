7f62fdc8ad79bf870e42d81c4434a4feedc084b6
zll600 <3400692417@qq.com>
2024-01-03 07:58:37 +0800

    feat: update oop

---
+++ oop/multiaccount.lua
@@ -0,0 +1,42 @@
+-- search k from the list
+local function search(k, plist)
+	for i = 1, #plist do
+		local v = plist[i][k]
+		if v then
+			return v
+		end
+	end
+end
+
+function createClass(...)
+	local c = {}
+	local parents = { ... }
+
+	-- find methods from the parents
+	setmetatable(c, {
+		__index = function(t, k)
+			return search(k, parents)
+		end,
+	})
+
+	c.__index = c
+
+	function c:new(o)
+		o = o or {}
+		setmetatable(o, c)
+		return c
+	end
+
+	return c
+end
+
+-- demo
+
+Named = {}
+function Named:getname()
+	return self.name
+end
+
+function Named:setname(name)
+	self.name = name
+end
---
+++ oop/single-method-object.lua
@@ -0,0 +1,11 @@
+function newObject(value)
+	return function(action, v)
+		if action == "get" then
+			return value
+		elseif action == "set" then
+			value = v
+		else
+			error("invalid action")
+		end
+	end
+end
